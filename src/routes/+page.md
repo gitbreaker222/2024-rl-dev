881cd4dc2c79887a642b5783320c9ecab134f174
Breaker222 <dummy@email.address>
2025-01-15 16:10:14 +0100

    installs mdsvex

---
+++ src/routes/+page.md
@@ -0,0 +1,8 @@
+---
+name: SvelteKit with MDsvex
+---
+
+# Welcome to {name}
+
+Visit [svelte.dev/docs/kit](https://svelte.dev/docs/kit) to read the documentation
+
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
 import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.md'],
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: ['.md'],
+			// Additional mdsvex configuration
+		})
+	],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
