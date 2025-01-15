6efae1200a79e7ec797c6b0aaa6bd1d4b9005f61
Breaker222 <dummy@email.address>
2025-01-15 15:02:18 +0100

    installs theme and fixes frontmatter access…
    + but does not fully read title var

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,3 @@
 <script>
 </script>
-
-_
 <slot></slot>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vitest/config';
+import { defaultTheme  } from '@sveltepress/theme-default' 
 import { sveltepress  } from '@sveltepress/vite' 
 
 export default defineConfig({
-	plugins: [sveltepress()],
+	plugins: [sveltepress({
+		theme : defaultTheme()
+	})],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
