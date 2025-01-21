39d73380ffbc3737c6959d415ad1d32d4d698110
Breaker222 <dummy@email.address>
2025-01-21 05:26:59 +0100

    updates and cleanup

--- routes/posts/{{year}}-{{month}}-{{day}}-{{slug}}/+page.md.md
+++
@@ -1,8 +0,0 @@
----
-layout: blog
-title: test blog
-date: 2025-01-16T16:42:00
-thumbnail: /images/uploads/Reconquista_Internet_Emblem.png
-rating: 3
----
-asdf
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+  const { children } = $props();
+  const routes = import.meta.glob('./**/*.md');
+
+  const postLinks = Object.keys(routes)
+    .filter((route) => route.includes('./posts'))
+    .map((route = '') => route.substr(1).replace('/+page.md', ''));
+  console.log('##', postLinks);
+</script>
+
+# home
+
+<a href="/">Home</a>
+
+# all posts:
+
+<ul>
+  {#each postLinks as post}
+    <li>
+      <a href={post}>{post.replace('/posts/', '')}</a>
+    </li>
+  {/each}
+</ul>
+
+{@render children()}
