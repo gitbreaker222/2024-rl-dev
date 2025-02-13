7487c12336f896e3bb250ba459437c2600cd3701
Breaker222 <dummy@email.address>
2025-02-13 06:28:54 +0100

    copy to en content to de

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,27 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
+const blogSchema = {
+	title: z.string(),
+	description: z.string(),
+	// Transform string to Date object
+	pubDate: z.coerce.date(),
+	updatedDate: z.coerce.date().optional(),
+	heroImage: z.string().optional(),
+}
+
 const blogEN = defineCollection({
 	// Load Markdown and MDX files in the `src/content/en/blog/` directory.
 	loader: glob({ base: './src/content/en/blog', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+	schema: z.object(blogSchema)
 });
 
-export const collections = { blogEN };
+const blogDE = defineCollection({
+	// Load Markdown and MDX files in the `src/content/de/blog/` directory.
+	loader: glob({ base: './src/content/de/blog', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object(blogSchema)
+});
+
+export const collections = { blogEN, blogDE };
