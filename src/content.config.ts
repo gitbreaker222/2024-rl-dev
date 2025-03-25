0368b52cdf01171735af77d639cd23da08958c59
Breaker222 <dummy@email.address>
2025-03-25 03:12:51 +0100

    ...progress

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,25 +1,11 @@
 import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
 
-const blogSchema = {
-	title: z.string(),
-	description: z.string(),
-	// Transform string to Date object
-	pubDate: z.coerce.date(),
-	updatedDate: z.coerce.date().optional(),
-	heroImage: z.string().optional(),
-}
+const markdownPage = defineCollection({
+	loader: glob({
+		base: `./src/content`,
+		pattern: '**/*.{md,mdx}',
+	})
+})
 
-const defineBlogCollection = (locale = 'en') => {
-	return defineCollection({
-		// Load Markdown and MDX files in the `src/content/en/blog/` directory.
-		loader: glob({ base: `./src/content/${locale}/blog`, pattern: '**/*.{md,mdx}' }),
-		// Type-check frontmatter using a schema
-		schema: z.object(blogSchema)
-	});
-}
-
-const blogEN = defineBlogCollection('en')
-const blogDE = defineBlogCollection('de')
-
-export const collections = { blogEN, blogDE };
+export const collections = { markdownPage };
