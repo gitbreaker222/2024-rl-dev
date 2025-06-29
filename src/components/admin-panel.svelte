cfeac8eb0dad46934894916e3bf7328e2df792e1
Breaker222 <dummy@email.address>
2025-06-30 00:23:01 +0200

    adds interactivity on client side

---
+++ src/components/admin-panel.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { Alert, Button } from "flowbite-svelte";
+  let showAlert = $state(false);
+
+  const toggleAlert = () => {
+    showAlert = !showAlert;
+  };
+</script>
+
+<Button
+  color="green"
+  onclick={toggleAlert}>Toggle Alert</Button
+>
+
+<div class="p-8">
+  {#if showAlert}
+    <Alert>
+      <span class="font-medium">Info alert!</span>
+      Change a few things up and try submitting again.
+    </Alert>
+  {/if}
+</div>
