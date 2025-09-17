79a4370e1425adb3a209432c9d25b6793250874a
Breaker222 <dummy@email.address>
2025-09-17 04:13:46 +0200

    makes meta tags and link preview personal

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'RL-DEV';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Hi, I\'m Ruben and I do Frontend+';
 export const RL_NAME = 'Ruben La Biunda';
 export const RL_MAIL = 'ruben.labiunda@posteo.de';
